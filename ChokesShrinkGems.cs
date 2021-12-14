f6076c47d8daa22996224f44480159b49beecb79
YoshiOG <m.yoshiog1@gmail.com>
2021-12-14 12:00:22 -0500

    Add "Chokes Shrink Gems" source code

---
+++ ChokesShrinkGems.cs
@@ -0,0 +1,130 @@
+﻿using BepInEx;
+using BiendeoCHLib;
+using BiendeoCHLib.Patches;
+using BiendeoCHLib.Patches.Attributes;
+using BiendeoCHLib.Wrappers;
+using BiendeoCHLib.Wrappers.Attributes;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.GUI;
+using Rewired;
+
+namespace ChokesShrinkGems
+{
+    [HarmonyCHPatch(typeof(BasePlayerWrapper), nameof(BasePlayerWrapper.MissNote))]
+    public class MissNoteHandler
+    {
+        [HarmonyCHPostfix]
+        static void Postfix(object __0)
+        {
+            var note = NoteWrapper.Wrap(__0);
+            ChokesShrinkGems.Instance.MissNotePostfix(note);
+        }
+    }
+
+    [BepInPlugin("com.yoshiog.clongemsizechoke", "ChokesShrinkGems", "1.0.1")]
+    [BepInDependency("com.biendeo.biendeochlib")]
+    public class ChokesShrinkGems : BaseUnityPlugin
+    {
+        /* // unnecessary stuff, might delete later
+        public class ThingsHiddenFromStreamWhenWritingCode
+        {
+            public static string scoreDisableThing = "[REDACTED]";
+        } 
+        // */
+        internal void MissNotePostfix(NoteWrapper note)
+        {
+            gemMult -= 1f;
+            if (gameMgr.BasePlayers[0].IsSPActive)
+            {
+                gemMult += 0.5f;
+            }
+            if (gemMult < 0f)
+            {
+                gemMult = 0f;
+            }
+            SetGemSize(gemMult);
+        }
+        public static ChokesShrinkGems Instance { get; private set; }
+        private Harmony Harmony;
+        public ChokesShrinkGems()
+        {
+            Instance = this;
+            Harmony = new Harmony("com.yoshiog.clongemsizechoke");
+            PatchBase.InitializePatches(Harmony, Assembly.GetExecutingAssembly(), Logger);
+        }
+        public GameManager gmObj;
+        public GameManagerWrapper gameMgr;
+        public float defGemSize;
+        public float gemMult;
+        public string sceneName;
+        public bool sceneChanged;
+        // private FieldInfo scoreDisableField;
+
+        public void SetGemSize(float percent = 100f)
+        {
+            foreach (var gameObj in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+            {
+                if (gameObj.name.StartsWith("Note"))
+                {
+                    gameObj.transform.localScale = new Vector3(defGemSize * percent / 100, defGemSize * percent / 100, 1f);
+                }
+                if (gameObj.name.StartsWith("Sustain(Clone)"))
+                {
+                    gameObj.transform.localScale = new Vector3(percent / 100, 1f, 1f);
+                }
+            }
+        }
+        public void Dbg_SetGemSizeToQuarter()
+        {
+            gemMult = 25f;
+            SetGemSize(25f);
+        }
+        public void Dbg_ResetGemSize()
+        {
+            gemMult = 100f;
+            SetGemSize();
+        }
+        #region Unity Methods
+        public void Start()
+        {
+            SceneManager.activeSceneChanged += delegate (Scene _, Scene __)
+            {
+                sceneChanged = true;
+            };
+            // scoreDisableField = typeof(GlobalVariables).GetField(ThingsHiddenFromStreamWhenWritingCode.scoreDisableThing);
+            defGemSize = 1.1f;
+            gemMult = 100f;
+            sceneName = "";
+        }
+        public void LateUpdate()
+        {
+            sceneName = SceneManager.GetActiveScene().name;
+            if (this.sceneChanged && gameMgr.IsNull())
+            {
+                gmObj = GameObject.Find("Game Manager").GetComponent<GameManager>();
+                gameMgr = GameManagerWrapper.Wrap(gmObj);
+                defGemSize = 1.1f * ((float)gameMgr.GlobalVariables.GameGemSize.CurrentValue / 100);
+                SetGemSize();
+                gemMult = 100f;
+                // // Disable score saving because reasons.  Might not be necessary.
+                // scoreDisableField.SetValue(gameMgr.GlobalVariables.GlobalVariables, true);
+                this.sceneChanged = false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ChokesShrinkGems")]
+[assembly: AssemblyDescription("Every missed note causes the gem size to shrink")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("YoshiOG")]
+[assembly: AssemblyProduct("ChokesShrinkGems")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5e0d8c56-1951-4ec6-baa9-b8f8c0f3a64f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
